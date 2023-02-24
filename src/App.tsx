fff70a186145e2d8e5ba0bbd818c22b5213bedf9
Eduard <eduartishchenkoseoppc@gmail.com>
2023-02-24 12:53:46 +0200

    2commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Todoolist } from './Todoolist';
+
+function App () {
+
+let tasks = [
+  {id: 1, title: "CSS", isDone: true},
+  {id: 2, title: "js", isDone: true},
+  {id: 3, title: "react", isDone: false},
+  {id: 4, title: "redux", isDone: false},
+]
+
+
+
+function removeTask(id:number) {
+  debugger
+  let resultTask = tasks.filter( () => true)
+  console.log(resultTask)
+
+
+}
+
+
+
+  return <div className="App">
+    <Todoolist title="Wat to learn" tasks={tasks}
+    removeTask={removeTask}
+    />
+    
+    
+
+  </div>
 }
 
 export default App;
+
+
+
